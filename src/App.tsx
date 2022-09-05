3fbf723c134f2fc430d9eda07fdf5919563d1474
Willian Louza <willianlouza@gmail.com>
2022-09-05 20:10:22 -0300

    build setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route, useParams, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Home from "./view/Home/index";
 import Inspector from "./view/Inspector";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/pokedex-react/" element={<Home />} />
         {/* <Route path="/pokemon/:id" element={<Inspector />} /> */}
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
 
   );
 }
