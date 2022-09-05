9c84aca5d854f58df6d7d98178a3579e5b938999
Willian Louza <willianlouza@gmail.com>
2022-09-05 19:52:08 -0300

    hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams, HashRouter } from "react-router-dom";
 import Home from "./view/Home/index";
 import Inspector from "./view/Inspector";
 
 function App() {
   return (
-
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/pokemon/:id" element={<Inspector />} /> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
